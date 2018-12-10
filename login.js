323edf905f1dd53b5cf5c96a7b7259696bec3f4c
Gleb <33223174+trust-in-core@users.noreply.github.com>
2018-12-10 13:26:26 +0200

    Add files via upload

--- login.js
+++ login.js
@@ -5,9 +5,8 @@ function login() {
     let log = document.getElementById('log');
     let pass = document.getElementById('pass');
 
-    if(log.value === "admin" && pass.value === "1234")
-        return redirect();
-    else if(log.value === "glebpodzin@gmail.com" && pass.value === "1111")
+    if(log.value === "admin" && pass.value === "1234"
+        || log.value === "glebpodzin@gmail.com" && pass.value === "1111")
         return redirect();
     else {
         log.value = "";
