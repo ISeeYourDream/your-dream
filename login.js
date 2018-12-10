d612734cfa9d315344d6c4e04e449f54dd755009
Gleb <33223174+trust-in-core@users.noreply.github.com>
2018-12-10 13:24:31 +0200

    Add files via upload

--- login.js
+++ login.js
@@ -1,16 +1,19 @@
 function redirect() {
     window.location.href = "profile.html";
 }
-function login(){
+function login() {
     let log = document.getElementById('log');
     let pass = document.getElementById('pass');
 
-    if (log.value === "Admin" && pass.value === "1234"
-        || log.value === "Gleb" && pass.value === "1111")
-            return redirect();
+    if(log.value === "admin" && pass.value === "1234")
+        return redirect();
+    else if(log.value === "glebpodzin@gmail.com" && pass.value === "1111")
+        return redirect();
     else {
         log.value = "";
         pass.value = "";
         alert("Incorrect login or password!");
     }
 }
+
+
